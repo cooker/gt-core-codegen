e73f1320ecdc0aab6c156e4690ee029059d14472
grant <fishkq@aliyun.com>
2020-03-09 11:22:04 +0800

    first commit

---
+++ maven-jooq-codegen-plugins/src/test/java/org/grant/zm/AppTest.java
@@ -0,0 +1,20 @@
+package org.grant.zm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
